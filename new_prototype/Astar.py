8bb98201f9d8cafe122217031e567f9385082952
graberyanis <yanisgraber006@gmail.com>
2025-09-13 20:01:45 +0200

    changed weighting, change path to be way based (instead of node)

--- new_prototype/Astar.py
+++ new_prototype/Astar.py
@@ -9,8 +9,8 @@ class Astar:
     def __init__(self, start_id:int, end_id:int):
         self.start:int = start_id
         self.end:int = end_id
-        self.explored_nodes:dict[int,dict[str,float|list[int]]] = {self.start: {"weight": 0, "path": []}}
-        # -> {node_id: {"weight": 1334, "path": [412342, 123432]}}
+        self.explored_nodes:dict[int,dict[str,float|list[str]]] = {self.start: {"weight": 0, "path": []}}
+        # -> {node_id: {"weight": 1334, "path": ["412342", "123432r"]}}
         self.active_nodes:list[int] = [self.start]
         self.time_searching = 0
         self.active:bool = True
@@ -21,12 +21,11 @@ class Astar:
             return
         if self.end in self.explored_nodes or self.active_nodes == [] or self.time_searching > 0.1:
             self.active = False
-            self.active_nodes = []
             return
         min_dist = math.inf
         node = None
         for node_check in self.active_nodes[:]:
-            dist = ((nodes[node_check].pos[0] - nodes[self.end].pos[0]) ** 2 + (nodes[node_check].pos[1] - nodes[self.end].pos[1]) ** 2)# + self.explored_nodes[node_check]["weight"]
+            dist = math.sqrt((nodes[node_check].pos[0] - nodes[self.end].pos[0]) ** 2 + (nodes[node_check].pos[1] - nodes[self.end].pos[1]) ** 2) + self.explored_nodes[node_check]["weight"]
             if  dist < min_dist:
                 min_dist = dist
                 node = node_check
@@ -38,18 +37,15 @@ class Astar:
 
                 self.active_nodes.append(new_node)
                 self.explored_nodes[new_node] = {"weight":self.explored_nodes[node]["weight"] + ways[way[0]].weights[way[1]],
-                                                "path":self.explored_nodes[node]["path"] + [new_node]}
+                                                "path":self.explored_nodes[node]["path"] + [way]}
         self.active_nodes.remove(node)
         self.time_searching += (time.perf_counter() - start)
 
 
-    def render(self, screen, scale, normalize, nodes, center, zoom, offset):
-        for node in self.explored_nodes:
-            pygame.draw.circle(screen, (255, 255, 0), scale(normalize(nodes[node].pos), center, zoom, offset), 5)
+    def render(self, screen, scale, nodes, center, zoom, offset):
+        #for node in self.explored_nodes:
+        #    pygame.draw.circle(screen, (255, 255, 0), scale(nodes[node].pos, center, zoom, offset), 5)
         for node in self.active_nodes:
-            pygame.draw.circle(screen, (255, 0, 255), scale(normalize(nodes[node].pos), center, zoom, offset), 5)
-        pygame.draw.circle(screen, (255, 0, 0), scale(normalize(nodes[self.start].pos), center, zoom, offset), 5)
-        pygame.draw.circle(screen, (0, 255, 0), scale(normalize(nodes[self.end].pos), center, zoom, offset), 5)
-        if self.end in self.explored_nodes:
-            for node in self.explored_nodes[self.end]["path"]:
-                pygame.draw.circle(screen, (0, 255, 255), scale(normalize(nodes[node].pos), center, zoom, offset), 5)
+            pygame.draw.circle(screen, (255, 255, 00), scale(nodes[node].pos, center, zoom, offset), 5)
+        pygame.draw.circle(screen, (255, 0, 0), scale(nodes[self.start].pos, center, zoom, offset), 5)
+        pygame.draw.circle(screen, (0, 255, 0), scale(nodes[self.end].pos, center, zoom, offset), 5)
