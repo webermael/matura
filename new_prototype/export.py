2f90d2bf2f6db3c15ac32cc22c6905c03ed3ec0b
graberyanis <yanisgraber006@gmail.com>
2025-09-18 16:16:09 +0200

    add entry ways to nodes instead of only exit ways

--- new_prototype/export.py
+++ new_prototype/export.py
@@ -1,7 +1,8 @@
 import osmnx as ox
 import json
+import re
 
-city = "Zürich, Switzerland"
+city = "Aargau, Switzerland"
 
 G = ox.graph_from_place(city, network_type="drive", simplify=False)
 
@@ -26,7 +27,8 @@ for id, data in G.nodes(data=True):
     export["nodes"][str(id)] = {
         "pos": (data["x"], data["y"]),
         "street_count": data["street_count"],
-        "ways": []
+        "ways": [],
+        "ways_in": []
         }
 
 
@@ -36,39 +38,42 @@ for start, end, data in G.edges(data=True):
     data["osmid"] = str(data["osmid"])
     if data["reversed"]:
         data["osmid"] += "r"
+    print(data.get("maxspeed", "50"))
+    speed = int(''.join(re.findall(r'\d+', str(data.get("maxspeed", "50").replace("rural", "80").replace("urban", "50").replace("signals", "50")))))
     if data["osmid"] not in export["ways"]:
         export["ways"][data["osmid"]] = {
         "id": data["osmid"],
         "reversed": data["reversed"],
         "lanes": int(data.get("lanes", 2)),
         "oneway": data.get("oneway", False),
-        "speed": int(data.get("maxspeed", 50)),
+        "speed": speed,
         "nodes": [[str(start), str(end)]],
-        "weights": [data["length"] / int(data.get("maxspeed", 50))],
+        "weights": [data["length"] / speed],
         "unconnected": []
         }
     else:
         if export["ways"][data["osmid"]]["nodes"][0][0] == str(end):
             if export["nodes"][str(end)]["street_count"] > 2:
                 export["ways"][data["osmid"]]["nodes"].insert(0, [str(start), str(end)])
-                export["ways"][data["osmid"]]["weights"].insert(0, data["length"] / int(data.get("maxspeed", 50)))
+                export["ways"][data["osmid"]]["weights"].insert(0, data["length"] / speed)
             
             else:
                 export["ways"][data["osmid"]]["nodes"][0].insert(0, str(start))
-                export["ways"][data["osmid"]]["weights"][0] += data["length"] / int(data.get("maxspeed", 50))
+                export["ways"][data["osmid"]]["weights"][0] += data["length"] / speed
 
         elif export["ways"][data["osmid"]]["nodes"][-1][-1] == str(start):
             if export["nodes"][str(start)]["street_count"] > 2:
                 export["ways"][data["osmid"]]["nodes"].append([str(start), str(end)])
-                export["ways"][data["osmid"]]["weights"].append(data["length"] / int(data.get("maxspeed", 50)))
+                export["ways"][data["osmid"]]["weights"].append(data["length"] / speed)
             else:
                 export["ways"][data["osmid"]]["nodes"][-1].append(str(end))
-                export["ways"][data["osmid"]]["weights"][-1] += data["length"] / int(data.get("maxspeed", 50))
+                export["ways"][data["osmid"]]["weights"][-1] += data["length"] / speed
         else:
-            export["ways"][data["osmid"]]["unconnected"].append([str(start), str(end), data["length"] / int(data.get("maxspeed", 50))])
+            export["ways"][data["osmid"]]["unconnected"].append([str(start), str(end), data["length"] / speed])
 
 for id, way in export["ways"].items():
     while way["unconnected"]:
+        connected = False
         for edge in way["unconnected"]:
 
             if export["ways"][id]["nodes"][0][0] == str(edge[1]):
@@ -79,6 +84,7 @@ for id, way in export["ways"].items():
                     export["ways"][id]["nodes"][0].insert(0, str(edge[0]))
                     export["ways"][id]["weights"][0] += edge[2]
                 way["unconnected"].remove(edge)
+                connected = True
 
             elif export["ways"][id]["nodes"][-1][-1] == str(edge[0]):
                 if export["nodes"][str(edge[0])]["street_count"] > 2:
@@ -88,10 +94,14 @@ for id, way in export["ways"].items():
                     export["ways"][id]["nodes"][-1].append(str(edge[1]))
                     export["ways"][id]["weights"][-1] += edge[2]
                 way["unconnected"].remove(edge)
+                connected = True
+        if not connected:
+            way["unconnected"] = []
 
     export["ways"][id].pop("unconnected")
     for segment in range(len(way["nodes"])):
         export["nodes"][way["nodes"][segment][0]]["ways"].append([id, segment])
+        export["nodes"][way["nodes"][segment][-1]]["ways_in"].append([id, segment])
 
 with open("matura\\new_prototype\\graph.json", "w") as f:
     json.dump(export, f, indent=2)
