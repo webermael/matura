b7960195182c3c663575091517a2335e5438a6c5
graberyanis <yanisgraber006@gmail.com>
2025-10-10 11:51:08 +0200

    split up drawing into multiple surfaces, add cars and A*

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,45 @@
 #include <SFML/Graphics.hpp>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <random>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 #include "display.h"
+#include "graph/astar.h"
 #include "graph/node.h"
 #include "graph/plot.h"
 #include "graph/way.h"
+#include "traffic/car.h"
 
 using json = nlohmann::json;
 
 int main() {
-  std::ifstream file("./src/Aarau.json");
-
-  json file_content = json::parse(file);
-
-  sf::Vector2u screen_size = {1920u, 1080u};
-  sf::Vector2f screen_center = {(float)screen_size.x / 2,
-                                (float)screen_size.y / 2};
+  // randomize seed
+  srand(std::chrono::system_clock::now().time_since_epoch().count());
 
   // --- IMPORT (maybe own file?) ---
-  Plot plot(screen_size, screen_center, file_content["bounds"]);
-  Display display(plot);
+  std::ifstream file("./src/Aarau.json");
+  json file_content = json::parse(file);
 
-  std::unordered_map<std::string, Node> nodes;
-
+  // create nodes
+  std::vector<Node> nodes;
+  std::unordered_map<std::string, std::size_t>
+      id_to_index;  // only for setting up ways after
   for (auto& [node_id, node] : file_content["nodes"].items()) {
-    sf::Vector2f pos = {node["pos"][0], node["pos"][1]};
-    sf::Vector2f display_pos = display.plot.to_screen_space(pos);
+    sf::Vector2<double> pos = {node["pos"][0], node["pos"][1]};
     int street_count = node["street_count"];
 
     std::vector<Way*> ways_in;
     std::vector<Way*> ways_out;
-
-    nodes.emplace(node_id, Node(node_id, pos, display_pos, street_count,
-                                ways_in, ways_out));
+    id_to_index.emplace(node_id, (nodes.size()));
+    nodes.push_back(Node(node_id, pos, street_count, ways_in, ways_out));
   }
 
+  // create ways
   std::vector<Way> ways;
   for (auto& [way_id, way] : file_content["ways"].items()) {
     for (size_t i = 0; i < way["nodes"].size(); i++) {
@@ -49,100 +49,170 @@ int main() {
       segment_nodes.reserve(segment.size());
 
       for (const auto& node_id : segment) {
-        segment_nodes.push_back(&nodes.at(node_id));
+        size_t index = id_to_index[node_id];
+        segment_nodes.push_back(&nodes[index]);
       }
 
       ways.emplace_back(way_id, i, way["oneway"], way["lanes"], way["turns"][i],
                         way["speed"], segment_nodes, way["weights"][i]);
     }
   }
+  id_to_index.clear();
 
+  // give way references to nodes
   for (auto& way : ways) {
     way.nodes[0]->ways_out.push_back(&way);  // push back adress (&) of way
     way.nodes.back()->ways_in.push_back(&way);
   }
 
-  // --- GAME LOOP ---
-  // auto window = sf::RenderWindow(sf::VideoMode({screen_size}), "Matura");
-  auto window =
-      sf::RenderWindow(sf::VideoMode(screen_size.x, screen_size.y), "Matura");
+  // create main display window
+  auto window = sf::RenderWindow(sf::VideoMode(1800, 900), "Matura");
+  sf::Vector2u interface_size = {200u, window.getSize().y};
+  sf::Vector2u screen_size(window.getSize().x - interface_size.x,
+                           window.getSize().y);
+  sf::Vector2f screen_center(screen_size.x / 2, screen_size.y / 2);
   window.setFramerateLimit(144);
+
+  // create surface for roads to be drawn
+  sf::RenderTexture road_surface;
+  road_surface.create(screen_size.x, screen_size.y);
+  sf::RenderTexture ui_surface;
+  ui_surface.create(screen_size.x, screen_size.y);
+  sf::RenderTexture car_surface;
+  car_surface.create(screen_size.x, screen_size.y);
+
+  // set up display handler
+  Plot plot(screen_size, screen_center, file_content["bounds"]);
+  Display display(plot);
+  display.reset_view(road_surface, window, nodes, ways);
+
+  // boot up A*
+  Astar astar;
+  std::vector<Node*> start_nodes;
+  std::vector<Node*> end_nodes;
+  for (size_t i = 0; i < nodes.size(); i++) {
+    if (nodes[i].street_count == 2 && nodes[i].ways_out.size() == 1 &&
+        (nodes[i].ways_out[0]->oneway && nodes[i].ways_in.size() == 0 ||
+         !nodes[i].ways_out[0]->oneway && nodes[i].ways_in.size() == 1)) {
+      start_nodes.push_back(nodes[i].ways_out[0]->nodes.front());
+    }
+    if (nodes[i].street_count == 2 && nodes[i].ways_in.size() == 1 &&
+        (nodes[i].ways_in[0]->oneway && nodes[i].ways_out.size() == 0 ||
+         !nodes[i].ways_in[0]->oneway && nodes[i].ways_out.size() == 1)) {
+      end_nodes.push_back(nodes[i].ways_in[0]->nodes.back());
+    }
+  }
+
+  // get cars ready
+  std::vector<std::unique_ptr<Car>> cars;
+  float car_reset = 0.5f;
+  float car_timer = car_reset;
+
   sf::Clock clock;
-
   float dt = 0.0f;
-  bool running = true;
-  bool selecting = false;
-  sf::Vector2i selection_start;
-  std::vector<float> rect_value(4);
+  std::vector<std::vector<Way*>> paths = {};
+  // --- GAME LOOP ---
+  while (window.isOpen()) {
+    bool mouse_clicked = false;
 
-  while (running) {  // window.isOpen()
-    // while (const std::optional event = window.pollEvent()) {
-    //   if (event->is<sf::Event::Closed>()) {
-    //     window.close();
-    //     running = false;
-    //   } else if (const auto* keyPressed =
-    //                  event->getIf<sf::Event::KeyPressed>()) {
-    //     if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
-    //       display.reset_view(nodes, ways);
-    //     }
-    //   }
-    // }
-
+    // --- EVENT HANDLING ---
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window.close();
-        running = false;
       } else if (event.type == sf::Event::KeyPressed &&
                  event.key.code == sf::Keyboard::Escape) {
-        display.reset_view(nodes, ways);
+        // escape to reset view
+        display.reset_view(road_surface, window, nodes, ways);
       }
+      if (event.type == sf::Event::MouseButtonPressed &&
+          event.mouseButton.button == sf::Mouse::Button::Left) {
+        mouse_clicked = true;
+      }
+      // maybe figure out something for resizing later
     }
 
-    window.clear(sf::Color::Black);
+    // --- UPDATING ---
 
-    for (auto& way : ways) {
-      if (way.is_visible) {
-        sf::VertexArray draw_way(sf::PrimitiveType::LineStrip);
-        for (auto& point : way.display_way) {
-          draw_way.append(sf::Vertex({point}));
-        }
-        window.draw(draw_way);
+    // check for mouse click (zooming)
+    display.update(road_surface, ui_surface, window, mouse_clicked, nodes,
+                   ways);
+
+    // run 10 steps of A*
+    if (astar.active) {
+      for (size_t i = 0; i < 10; i++) {
+        astar.step();
       }
-    }
-
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-      sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
-      if (!selecting) {
-        selecting = true;
-        selection_start = mouse_pos;
-      }
-      float t = std::min(selection_start.y, mouse_pos.y);
-      float l = std::min(selection_start.x, mouse_pos.x);
-      float b = std::min(selection_start.y, mouse_pos.y);
-      float r = std::min(selection_start.x, mouse_pos.x);
-
-      rect_value[0] = t;
-      rect_value[1] = l;
-      rect_value[2] = b;
-      rect_value[3] = r;
-
-      sf::RectangleShape rectangle({r - l, b - t});
-      rectangle.setPosition({l, t});
-      rectangle.setFillColor(sf::Color::Red);
-      rectangle.setOutlineThickness(10.0f);
-      window.draw(rectangle);
-      std::cout << "Drew Rect" << std::endl;
     } else {
-      if (selecting) {
-        selecting = false;
-        display.set_view(nodes, ways, rect_value);
+      // if new path is found, add to list
+      if (astar.explored_nodes.count(astar.end) &&
+          !std::count(paths.begin(), paths.end(),
+                      astar.explored_nodes[astar.end].path) &&
+          !(astar.start == astar.end)) {
+        paths.push_back(astar.explored_nodes[astar.end].path);
+      }
+      // start next search
+      astar.reset(start_nodes[rand() % start_nodes.size()],
+                  end_nodes[rand() % end_nodes.size()]);
+    }
+
+    // spawn new car if timer depletes
+    car_timer -= dt;
+    while (car_timer < 0) {
+      car_timer += car_reset;
+      if (paths.size() > 0) {
+        std::vector<Way*> path = paths[rand() % paths.size()];
+        cars.emplace_back(std::make_unique<Car>(path));
       }
     }
-    // --- current translation status ---
+    car_surface.clear(sf::Color::Transparent);
+    for (auto& car_ptr : cars) {
+      car_ptr->update(dt, ACCELERATION, car_surface, display);
+      car_ptr->render(car_surface, display);
+    }
+    // remove finished cars
+    // Remove all inactive cars
+    cars.erase(std::remove_if(cars.begin(), cars.end(),
+                              [](const std::unique_ptr<Car>& car) {
+                                return !car->active;
+                              }),
+               cars.end());
+
+    // sort cars in each way according to their progress
+    for (auto& way : ways) {
+      std::sort(way.cars.begin(), way.cars.end(), [](Car* a, Car* b) {
+        return a->way_progress < b->way_progress;
+      });
+    }
+
+    // --- DRAWING ---
+    window.clear(sf::Color::White);
+    // turn road surface into a sprite and draw it
+    road_surface.clear(sf::Color::Black);
+    for (auto& way : ways) {
+      if (way.cars.size() > 0) {
+        display.draw_polyline(
+            road_surface, way,
+            7.f * display.plot.scale * (float)way.lanes +
+                display.plot.scale * 3.f,
+            sf::Color(std::min(255ull, (way.cars.size() * 50)), 100, 100));
+      } else {
+        display.draw_polyline(road_surface, way,
+                              7.f * display.plot.scale * (float)way.lanes,
+                              sf::Color(100, 100, 100));
+      }
+    }
+    sf::Sprite road_sprite(road_surface.getTexture());
+    window.draw(road_sprite);
+
+    sf::Sprite car_sprite(car_surface.getTexture());
+    window.draw(car_sprite);
+    // draw the red selection rectangle on top of everything
+    sf::Sprite ui_sprite(ui_surface.getTexture());
+    window.draw(ui_sprite);
 
     dt = clock.restart().asSeconds();
-
+    // show the frame
     window.display();
   }
 }
