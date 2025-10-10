d0eae7d8bd28bdea80fb73b6568b039a76f796a2
graberyanis <yanisgraber006@gmail.com>
2025-10-10 11:42:47 +0200

    change position to vector2<double>

--- src/graph/node.h
+++ src/graph/node.h
@@ -8,31 +8,16 @@ class Way;
 class Node {
  public:
   std::string id;
-  sf::Vector2f pos;
-  sf::Vector2f display_pos;
+  sf::Vector2<double> pos;
   int street_count;
   std::vector<Way*> ways_out;
   std::vector<Way*> ways_in;
-  bool is_visible;
 
-  Node(std::string id, sf::Vector2f pos, sf::Vector2f display_pos,
-       int street_count, std::vector<Way*>& ways_out,
-       std::vector<Way*>& ways_in)
+  Node(std::string id, sf::Vector2<double> pos, int street_count,
+       std::vector<Way*>& ways_out, std::vector<Way*>& ways_in)
       : id(id),
         pos(pos),
-        display_pos(display_pos),
         street_count(street_count),
         ways_out(ways_out),
-        ways_in(ways_in) {
-    is_visible = true;
-  }
-
-  void test_visible(sf::Vector2u screen_size) {
-    if (0 < display_pos.x < screen_size.x &&
-        0 < display_pos.y < screen_size.y) {
-      is_visible = true;
-    } else {
-      is_visible = true;
-    }
-  }
+        ways_in(ways_in) {}
 };
