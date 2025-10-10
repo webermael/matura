17be6705b0c1eda9d8fd63c45d67b405da0842af
graberyanis <yanisgraber006@gmail.com>
2025-10-10 11:43:55 +0200

    change world pos to vector<double>

--- src/graph/plot.h
+++ src/graph/plot.h
@@ -12,8 +12,8 @@ class Plot {
   sf::Vector2f screen_center;
   std::unordered_map<std::string, float> bounds;
 
-  sf::Vector2f translation;
-  sf::Vector2f graph_size;
+  sf::Vector2<double> translation;
+  sf::Vector2<double> graph_size;
   float scale;
 
   void reset_values() {
@@ -30,14 +30,17 @@ class Plot {
     reset_values();
   }
 
-  sf::Vector2f to_screen_space(sf::Vector2f position) {
-    return {(position.x - translation.x - graph_size.x / 2) * scale +
-                screen_center.x,
-            (position.y - translation.y - graph_size.y / 2) * -scale +
-                screen_center.y};
+  sf::Vector2f to_screen_space(sf::Vector2<double> position) {
+    return sf::Vector2f(
+        static_cast<float>((position.x - translation.x - graph_size.x / 2.0) *
+                               scale +
+                           screen_center.x),
+        static_cast<float>((position.y - translation.y - graph_size.y / 2.0) *
+                               scale +
+                           screen_center.y));
   }
 
-  sf::Vector2f from_screen_space(sf::Vector2f position) {
+  sf::Vector2<double> from_screen_space(sf::Vector2f position) {
     return {(position.x - screen_center.x) / scale + translation.x +
                 graph_size.x / 2,
             (position.y - screen_center.y) / -scale + translation.y +
@@ -45,8 +48,8 @@ class Plot {
   }
 
   void set_values(float top, float left, float bottom, float right) {
-    sf::Vector2f temp_translation;
-    sf::Vector2f temp_corner;
+    sf::Vector2<double> temp_translation;
+    sf::Vector2<double> temp_corner;
     if (std::abs(bottom - top) > 0 && std::abs(right - left) > 0) {
       temp_translation = from_screen_space({left, bottom});
       temp_corner = from_screen_space({right, top});
